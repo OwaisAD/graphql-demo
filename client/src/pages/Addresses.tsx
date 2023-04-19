1b1be3cb2a53c51f0c3c6525a4c849aff71740e3
Owais Dashti <owaisdashti@outlook.com>
2023-04-19 13:16:48 +0200

    added ThemeProvider

--- client/src/pages/Addresses.tsx
+++ client/src/pages/Addresses.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import CreateAddressForm from "../components/CreateAddressForm";
 import AddressList from "../components/Addresslist";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Addresses = () => {
+  const { isLight, light, dark } = useContext(ThemeContext);
+  const theme = isLight ? light : dark;
   return (
-    <div>
+    <div
+      style={{
+        background: theme.ui,
+        color: theme.text,
+        padding: "20px",
+        marginTop: "10px",
+        borderRadius: "12px",
+      }}
+    >
       <h1>Addresses page</h1>
       <CreateAddressForm />
       <AddressList />
--- client/src/pages/Persons.tsx
+++ client/src/pages/Persons.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import CreatePersonForm from "../components/CreatePersonForm";
 import PersonList from "../components/Personlist";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Persons = () => {
+  const { isLight, light, dark } = useContext(ThemeContext);
+  const theme = isLight ? light : dark;
   return (
-    <div>
+    <div
+      style={{
+        background: theme.ui,
+        color: theme.text,
+        padding: "20px",
+        marginTop: "10px",
+        borderRadius: "12px",
+      }}
+    >
       <h1>Persons page</h1>
       <CreatePersonForm />
       <PersonList />
